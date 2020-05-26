d0d590df5e3700d0e043031622825a74a687c3e7
Juan Francisco Maldonado Leon <juan.maldonado.leon@gmail.com>
2020-05-26 10:40:22 -0400

    modal

--- src/app/components/public/inicio/inicio.component.ts
+++ src/app/components/public/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private modalService: NgbModal
+  ) { }
 
   ngOnInit(): void {
+  }
+
+
+  // Abrir Popup (Modal)
+  openModal( content ){
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
   }
 
 }
